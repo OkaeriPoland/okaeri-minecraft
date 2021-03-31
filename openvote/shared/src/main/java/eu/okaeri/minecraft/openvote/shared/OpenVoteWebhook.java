874fd7a18528b3e1d16560f8caf74433154a2d0f
PinkLolicorn <pinklolicorngithub@daffit.pl>
2021-03-31 20:36:40 +0200

    Update license headers

--- openvote/shared/src/main/java/eu/okaeri/minecraft/openvote/shared/OpenVoteWebhook.java
+++ openvote/shared/src/main/java/eu/okaeri/minecraft/openvote/shared/OpenVoteWebhook.java
@@ -1,6 +1,6 @@
 /*
- * OK! No.Proxy Minecraft
- * Copyright (C) 2020 Okaeri, Dawid Sawicki
+ * OK! OpenVote Minecraft
+ * Copyright (C) 2021 Okaeri, Dawid Sawicki
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
